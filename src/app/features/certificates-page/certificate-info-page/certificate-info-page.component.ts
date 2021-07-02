e91bcb70ef2b9a528e642d92ff090f6ae005f7ed
Egor <fromzer@mail.ru>
2021-07-02 08:29:05 +0300

    small fix

--- src/app/features/certificates-page/certificate-info-page/certificate-info-page.component.ts
+++ src/app/features/certificates-page/certificate-info-page/certificate-info-page.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Params} from "@angular/router";
 import {Certificate} from "../../../shared/interfaces";
 import {CertificatesService} from "../../../shared/service/certificates.service";
 import {Observable} from "rxjs";
+import {MaterialService} from "../../../shared/classes/material.service";
+import {CartService} from "../../../shared/service/cart.service";
 
 @Component({
   selector: 'app-certificate-info-page',
@@ -13,12 +15,19 @@ export class CertificateInfoPageComponent implements OnInit {
   certificate$: Observable<Certificate> | undefined
 
   constructor(private route: ActivatedRoute,
-    private certificateService: CertificatesService) { }
+              private certificateService: CertificatesService,
+              private cartService: CartService) {
+  }
 
   // @ts-ignore
   ngOnInit() {
     this.route.params.subscribe((param: Params) => {
       this.certificate$ = this.certificateService.getCertificateById(param['id'])
     })
+  }
+
+  addToCart(certificate: Certificate) {
+    MaterialService.toast('Certificate added to cart')
+    this.cartService.addToCart(certificate);
   }
 }
